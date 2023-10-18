3279bd7e90401d1b4cc700383cd3eff00c508570
borgesiris <bborgesiris.04@gmail.com>
2023-10-17 21:52:52 -0300

    add aula02 de JS

---
+++ js/Aula02-Variaveis/main.js
@@ -0,0 +1,55 @@
+var nomeDoPrimeiroAluno = "CnPJOTO"
+
+console.log(nomeDoPrimeiroAluno)
+
+let numeroDeAprovados = 5
+
+console.log(numeroDeAprovados)
+
+console.clear()
+
+var nomeDoAluno = ("Felipe")
+
+nomeDoAluno = ("Victor")
+
+console.log(nomeDoAluno)
+
+// Exibir a soma de dois números 
+// console.log (2+4)
+
+
+var numero1 = 2;
+
+var numero2 = 4;
+
+var soma = numero1 + numero2;
+
+console.log(soma)
+
+// String 
+
+var texto = "Eu gosto de pipoco ";
+
+var texto2 = "e eu gosto de pipoca";
+
+// Concatenar - ato de juntar duas ou mais strings \n é usado para quebrar linhas
+
+var Concatenar = texto + "\n" + texto2;
+
+console.log(Concatenar);
+
+console.clear
+
+// Valores Boleanos (True ou False)
+
+var osDeVerdadeEuSeiQuemSao = false;
+
+// Valores Vazios (Null e undefined)
+
+// Conversão Automática, operação com tipos deiferentes de dados, Ex: 8*null
+
+// soma de string com númerico
+
+console.log("5" + 1) 
+
+// operadores 
