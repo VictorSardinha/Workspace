ad30db4b173fe08013ff94b50089f549b99c1a54
borgesiris <bborgesiris.04@gmail.com>
2023-10-19 18:57:56 -0300

    add ex de java script aula 02

---
+++ js/Aula02-Variaveis/ExCasa/main.js
@@ -0,0 +1,11 @@
+var numero = 10;
+
+var numero2 = 9;
+
+var numero3 = 7;
+
+var numero4 = 8;
+
+var media = (numero + numero2 + numero3 + numero4) /4;
+
+console.log(media);
--- js/Aula02-Variaveis/main.js
+++ js/Aula02-Variaveis/main.js
@@ -53,3 +53,35 @@ var osDeVerdadeEuSeiQuemSao = false;
 console.log("5" + 1) 
 
 // operadores 
+
+console.clear() //estamos limpando todo o conteudo
+
+
+var incremento = 2; // estamos declarando a variavel que esta recebendo o valor de 2
+
+
+incremento=28 // estamos redeclarando a variavel e acrescentando um valor a mais, ou seja tera uma soma entre o valor anterior e o valor atual que acabei de acrescentar
+
+
+ console.log(incremento)
+
+ console.clear() //estamos limpando o conteudo
+
+var imparPar = 3%2 // estamos declarando uma variavel o programa esta tentando identifcar se o resto da divisão é Impar ou Par console.log("se o resultado for 1 então o numero é Impar de o valor for e então o valor é par, esse então é o seu resultado" + imparPar) // estamos usando a concatenação para mo 
+
+ console.clear() // estamos limpando o conteudo
+
+ var comparacao = 1>0 // estamos declarando uma variavel e nela estamos tentando encontrar um valor verdadeiro ou falso
+
+
+console.log("se o valor foi maior ou menor que ele mesmo, retornar um valor TRUE ou FALSE, logo o resultado é: " + comparacao)
+
+console.clear() //estamos limpando o conteudo 
+
+
+var dois5inaisDelgual = 2 == 3
+
+console.log(doisSinaisDelgual)
+
+
+console.clear() //estamos limpando o conteudo
