ee1765af936384e35005498e5f04c9793d980ae1
borgesiris <bborgesiris.04@gmail.com>
2023-10-19 21:48:00 -0300

    add aula de java 03

---
+++ js/Aula03-strings/main.js
@@ -0,0 +1,59 @@
+var texto1 = "Texto aleatorio dos atrasados da volta pausa";
+
+var texto2 = "Texto aleatorio dos atrasados da volta pausa";
+
+console.log(texto1 + " " + "A vida de sapinho dando um pulinho na água" + " " + texto2)
+
+// Template string
+
+console.log(`${texto1} A vida de um sapinho pulando na água ${texto2}`)
+
+var numero1 = 2;
+var numero2 = 3;
+
+console.log(`${numero1 + numero2}`)
+console.log(numero1 + numero2)
+
+console.clear()
+
+const string01 = "Instituto da oportundade Social"
+
+console.log(string01);
+console.log(string01.charAt(10));
+
+console.clear()
+
+const string02 = "Instituto da oportundade Social"
+
+console.log("Quantidade de caracteres:" + string02.length)
+
+const string03 = "Instituto da Oportunidade Social"
+
+console.log("trazer todo o texto em maiúsculo:" + string03.toLocaleUpperCase);
+
+string04 = "INSTITUTO DA OPORTUNIDADE SOCIAL"
+
+console.log("Trazer tudo em minúsculo:" + string04.toLowerCase)
+
+const str = "Mozilla";
+
+console.log (str.substring(1,3));
+// Saída "oz"
+
+console.log(str.substring(2));
+// Saída "zilla"
+
+var trocaTexto = "Bem vindo ao jogo, Roblox"
+
+let textoTrocado = trocaTexto.replace ("Roblox", "PAO_DE_FORMA");
+
+console.log("Texto original:" + trocaTexto);
+console.log("Texto Alterado:" + textoTrocado);
+
+console.clear()
+
+var retirandoEspaços = "          A vida é meio complicada as vezes           "
+
+console.log(retirandoEspaços.trim())
+
+
