c8ace562412610cdc7c664f9a8d1f65fbc443144
Victor Sardinha <Vmasardinhapsn@gmail.com>
2023-10-21 21:17:13 -0300

    add exercicio feito de java aula 03

---
+++ js/Aula03-strings/ExCasa/main.js
@@ -0,0 +1,14 @@
+// Crie a string
+let minhaString = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+// Exiba o caractere na posição 19
+console.log("O caractere na posição 19: " + minhaString[19]);
+
+// Exiba o tamanho da string
+console.log("O tamanho de toda a String: " + minhaString.length);
+
+// Exiba a string em maiúsculo
+console.log("Toda a String em maiúsculo: " + minhaString.toUpperCase());
+
+// Exiba apenas a palavra "Devs"
+console.log("Apenas a palavra Devs: " + minhaString.substring(minhaString.lastIndexOf("Devs")));
