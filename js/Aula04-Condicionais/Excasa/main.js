6872b406876dd5c5fbb87f539b1025160fc1af1a
borgesiris <bborgesiris.04@gmail.com>
2023-10-26 18:51:54 -0300

    add ex casa da aula 05 e 04

--- js/Aula04-Condicionais/Excasa/main.js
+++ js/Aula04-Condicionais/Excasa/main.js
@@ -1,1 +1,12 @@
-  
+ // Leia os números inteiros
+var numero1 = 50;
+var numero2 = 45;
+
+// Verifique qual número é maior e qual é menor
+if (numero1 > numero2) {
+    console.log("numero1 é maior que numero2");
+} else if (numero1 < numero2) {
+    console.log("numero1 é menor que numero2");
+} else {
+    console.log("numero1 e numero2 são iguais");
+} 
---
+++ js/Aula05-Repetição/exCasa/main.js
@@ -0,0 +1,6 @@
+
+var numero = 5;
+for (var i = 1; i <= 10; i++) {
+    var resultado = numero * i;
+    console.log(numero + " x " + i + " = " + resultado);
+}
