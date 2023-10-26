be3f70cb8c9094efbcbf8314e25454e36053687f
borgesiris <bborgesiris.04@gmail.com>
2023-10-25 21:44:17 -0300

    fazer ex casa da aula 04 e 05, add aula 05 de js

---
+++ js/Aula05-Repetição/main.js
@@ -0,0 +1,19 @@
+// Laços while
+// let contador = 10;
+
+// while(contador >= 0){//  true    
+//     console.log(`O número do contador é ${contador}`)
+//     contador--;
+// }
+
+for (let contador = 1; contador <=10; contador++){
+    console.log(`O número do contador é ${contador}`)
+}
+
+// let contador = 1
+// do{
+
+//     debugger
+//     console.log(`O número do contador é ${contador}`)
+//     contador++;
+// }while(contador <= 10)
