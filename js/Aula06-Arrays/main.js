9199c43a9f9ea2ea2563a3ec09522460a5e1ed61
borgesiris <bborgesiris.04@gmail.com>
2023-10-26 21:51:36 -0300

    add aula 06 de js

---
+++ js/Aula06-Arrays/main.js
@@ -0,0 +1,63 @@
+var numeros = [54, 111, 58, 853, 17, 75, 30]
+// Array Unidimensional
+
+console.log("antes:" + numeros)
+numeros[3] = "hora do lanche"
+console.log("Depois:" + numeros)
+
+// console.clear()
+
+var matrix = [
+    ["ICarly", 2011, 10],
+    ["Os Fet de EverlyPlace", 2009,10],
+    ["Doug",1999, 8.7]
+]
+
+// console.log(matrix)
+console.log("Antes" + matrix)
+matrix[2][1] = 1800
+console.log("Depois" + matrix) 
+
+console.clear()
+
+var numArray = [2,3,6,7,45,86]
+console.log(numArray)
+// Metodo para deletar
+delete numArray[3]
+console.log(numArray)
+
+// Metodo para add no inicio
+// let inseririnicio = numArray.unshift("Pipoca")
+// console.log(numArray)
+// console.log(inseririnicio)
+
+// Metodo de remover uma informação no inicio do array
+// let retiraInicio = numArray.shift()
+// console.log(numArray)
+// console.log(retiraInicio)
+
+
+// Metofo ADD nova informação no final do Array
+// let novaInformacao = numArray.push("Banana")
+// console.log(numArray)
+// console.log(novaInformacao)
+
+// Metodo para retirar o ultimo elemento
+// let retiraUltimoElemento = numArray.pop() //86
+// console.log(retiraUltimoElemento)
+// console.log(numArray)
+
+// retornar o tamanho do Array
+// console.log(numArray.length)
+
+// Metodo Join
+// let testeJoin = numArray.join(".")
+// console.log(testeJoin)
+
+// Metodo toString
+// let receba = numArray.toString()
+// console.log(receba)
+
+
+
+
