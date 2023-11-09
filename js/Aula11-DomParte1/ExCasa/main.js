158becf2824d2dc4b72f29097a0cfa37283f7997
borgesiris <bborgesiris.04@gmail.com>
2023-11-08 21:13:21 -0300

    add aula e ex da aula 11 de java script

---
+++ js/Aula11-DomParte1/ExCasa/main.js
@@ -0,0 +1,9 @@
+// Criação da tag h1
+const elementoH1 = document.createElement('h1');
+elementoH1.textContent = 'Cadastro de Usuário';
+
+// Inserção da tag h1 no documento
+document.body.appendChild(elementoH1);
+
+// título da página
+elementoH1.innerHTML = 'Cadastro Empresa Macron';
---
+++ js/Aula11-DomParte1/main.js
@@ -0,0 +1,10 @@
+// Criação da variavel e criação do botão
+let botao = document.createElement("button")
+// Adicionar o filho(botao) na body
+document.body.appendChild(botao)
+// adicionando texto detro do botão
+botao.innerHTML = "Clica aqui seu safado"
+// adcionando um estilo
+botao.style.borderBottom = "solid 3px blue"
+// Adicionando uma cor de fundo
+document.body.style.backgroundColor = "Black"
