645a2ff0d0f2b615e5fe99045840cbd4c8001e74
borgesiris <bborgesiris.04@gmail.com>
2023-11-10 20:31:24 -0300

    add aula 13 de DOM

---
+++ js/Aula13-DOMParte3/main.js
@@ -0,0 +1,12 @@
+let pipoca = document.querySelectorAll("p");
+console.log(pipoca);
+pipoca[0].style.color = "pink"
+pipoca[1].style.color = "pink"
+pipoca[2].style.color = "pink"
+pipoca[3].style.color = "pink"
+
+let j = document.querySelector(".exemplo");
+j.innerHTML = "aqui o texto foi alterado jubileu";
+j.style.color = "blue"
+
+let y = document.querySelector("#exemplo2").style.color = "yellow"m   
