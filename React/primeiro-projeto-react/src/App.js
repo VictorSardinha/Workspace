decebec12247bea8266bb0ea5acd2d8d99ded65f
borgesiris <bborgesiris.04@gmail.com>
2023-11-17 20:45:53 -0300

    add continuação de react

--- React/primeiro-projeto-react/src/App.js
+++ React/primeiro-projeto-react/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Exemplo from './components/Exemplo';
+import BoasVindas from './components/BoasVindas';
 function App() {
   return (
     <div className="App">
+      <BoasVindas/>
     <Exemplo/>
     </div>
   );
--- React/primeiro-projeto-react/src/components/Exemplo.jsx
+++ React/primeiro-projeto-react/src/components/Exemplo.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-const Exemplo = <h1>Estou apodrecendo por dentro </h1>
+let estilo = {listStyle: 'none', color: 'blue', fontSize: '67px', textAlign: 'center'}
+
+
+const Exemplo = () => {
+  return (
+    <>
+      <h1>Sexta-feira depressiva</h1>
+      <p>Metade vai repravar</p>
+      <ul style={estilo}>
+        <li>Maria Eduarda</li>
+        <li>Anna Lima</li>
+        <li>Victor Sardinha</li>
+        <li>O restante da sala</li>
+      </ul>
+    </>
+  );
+};
 
 export default Exemplo;
